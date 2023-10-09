ff185460bd4b3aec9f4fe067e88943e5cee259f5
Jingkun Feng <jingkun_feng@outlook.com>
2023-10-09 08:02:01 +0200

    [add] global configs

---
+++ scripts/run_vo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+python -B experiment/rgbd_vo.py \
+--dataset TUM_RGBD \
+--encoder_name RGB \
+--mestimator None \
+--solver Direct-Nodamping \
+--cpu_workers 12 \
+--batch_per_gpu 96 \
+--feature_channel 8 \
+--feature_extract conv \
+--uncertainty laplacian \
+--uncertainty_channel 1 \
+--direction inverse \
+--init_pose sfm_net \
+--train_init_pose \
+--multi_hypo prob_fuse \
+--remove_tru_sigma \
+--checkpoint checkpoint/dynamic_finetune_checkpoint_epoch39.pth.tar \
+--vo RGB \
+--image_resize 0.25 \
+--time \
+--vis_feat
+
+# --combine_ICP \
+
+# --max_iter_per_pyr 20
+
+# keyframe tracking visualization
+# --vo_type keyframe --two_view
